f092d88dbed1be70152b24a4f2e502bf8caa78b4
Kolyasick <igrokfaceit21@gmail.com>
2025-10-26 16:04:02 +0300

    delete favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,23 +67,4 @@ export default defineNuxtConfig({
       },
     },
   },
-  app: {
-    head: {
-      link: [
-        { rel: "apple-touch-icon", sizes: "57x57", href: "https://magnatmedia.com/fav/apple-icon-57x57.png" },
-        { rel: "apple-touch-icon", sizes: "60x60", href: "https://magnatmedia.com/fav/apple-icon-60x60.png" },
-        { rel: "apple-touch-icon", sizes: "72x72", href: "https://magnatmedia.com/fav/apple-icon-72x72.png" },
-        { rel: "apple-touch-icon", sizes: "76x76", href: "https://magnatmedia.com/fav/apple-icon-76x76.png" },
-        { rel: "apple-touch-icon", sizes: "114x114", href: "https://magnatmedia.com/fav/apple-icon-114x114.png" },
-        { rel: "apple-touch-icon", sizes: "120x120", href: "https://magnatmedia.com/fav/apple-icon-120x120.png" },
-        { rel: "apple-touch-icon", sizes: "144x144", href: "https://magnatmedia.com/fav/apple-icon-144x144.png" },
-        { rel: "apple-touch-icon", sizes: "152x152", href: "https://magnatmedia.com/fav/apple-icon-152x152.png" },
-        { rel: "apple-touch-icon", sizes: "180x180", href: "https://magnatmedia.com/fav/apple-icon-180x180.png" },
-        { rel: "icon", type: "image/png", sizes: "192x192", href: "https://magnatmedia.com/fav/android-icon-192x192.png" },
-        { rel: "icon", type: "image/png", sizes: "32x32", href: "https://magnatmedia.com/fav/favicon-32x32.png" },
-        { rel: "icon", type: "image/png", sizes: "96x96", href: "https://magnatmedia.com/fav/favicon-96x96.png" },
-        { rel: "icon", type: "image/png", sizes: "16x16", href: "https://magnatmedia.com/fav/favicon-16x16.png" },
-      ],
-    },
-  },
 });
